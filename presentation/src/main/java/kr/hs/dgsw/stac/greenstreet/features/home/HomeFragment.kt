2f3320d9b3d3ab270188c530e280c3c6a14ac804
snack655 <cmjcmj1230@gmail.com>
2022-07-28 23:53:24 +0900

    Add : 지도 기본을 세팅하였습니다. 권한, 줌 사이즈, 포커스 모드 등

--- presentation/src/main/java/kr/hs/dgsw/stac/greenstreet/features/home/HomeFragment.kt
+++ presentation/src/main/java/kr/hs/dgsw/stac/greenstreet/features/home/HomeFragment.kt
@@ -2,8 +2,10 @@ package kr.hs.dgsw.stac.greenstreet.features.home
 
 import android.os.Bundle
 import androidx.fragment.app.viewModels
+import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 import kr.hs.dgsw.stac.greenstreet.R
 import kr.hs.dgsw.stac.greenstreet.base.BaseFragment
 import kr.hs.dgsw.stac.greenstreet.databinding.FragmentHomeBinding
@@ -12,15 +14,44 @@ import kr.hs.dgsw.stac.greenstreet.databinding.FragmentHomeBinding
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.fragment_home), OnMapReadyCallback {
     override val viewModel: HomeViewModel by viewModels()
     override val hasBottomNav: Boolean = true
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
 
     override fun start() {
         binding.mapView.onCreate(savedInstanceState)
         binding.mapView.getMapAsync(this)
     }
 
-    override fun bindingViewEvent() {}
-    override fun onMapReady(map: NaverMap) {}
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return
+        }
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated) {
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+    }
 
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        naverMap.uiSettings.isLocationButtonEnabled = true
+        naverMap.maxZoom = 18.0
+        naverMap.minZoom = 10.0
+    }
+
+    override fun bindingViewEvent() {}
+
+    // 아래 수명주기 연결
     override fun onStart() {
         super.onStart()
         binding.mapView.onStart()
@@ -54,5 +85,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.f
     override fun onLowMemory() {
         super.onLowMemory()
         binding.mapView.onLowMemory()
+    }
+
+    companion object {
+        private val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
