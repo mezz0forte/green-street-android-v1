43a213353e8f020d1c17487ac35bb87dfddcb6ec
LucyMoon <rjf5907@naver.com>
2022-08-29 07:46:12 +0900

    Add: 게시물 추가 코드 작성

--- presentation/src/main/java/kr/hs/dgsw/stac/greenstreet/features/post/add/AddPostViewModel.kt
+++ presentation/src/main/java/kr/hs/dgsw/stac/greenstreet/features/post/add/AddPostViewModel.kt
@@ -1,14 +1,32 @@
 package kr.hs.dgsw.stac.greenstreet.features.post.add
 
+import android.net.Uri
+import androidx.lifecycle.MutableLiveData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kr.hs.dgsw.stac.domain.usecase.posting.PostPosting
+import kr.hs.dgsw.stac.domain.usecase.posting.PostingUseCases
 import kr.hs.dgsw.stac.greenstreet.base.BaseViewModel
+import javax.inject.Inject
 
 class AddPostViewModel : BaseViewModel() {
+
+    val imageList = MutableLiveData<List<Uri>>()
 
     fun onClickBack() {
         viewEvent(EVENT_ON_CLICK_BACK)
     }
 
+    fun onClickAddimg() {
+        viewEvent(EVENT_ON_CLICK_ADDIMG)
+    }
+
+    fun onClickNext() {
+        viewEvent(EVENT_ON_CLICK_NEXT)
+    }
+
     companion object {
         const val EVENT_ON_CLICK_BACK = 0
+        const val EVENT_ON_CLICK_NEXT = 1
+        const val EVENT_ON_CLICK_ADDIMG = 2
     }
 }
