8c6d1fc0a39e5b4aaec5d85ac67051c55f66df5b
최민재 <cmjcmj1230@gmail.com>
2022-09-22 20:21:32 +0900

    Add : Add Solution screen

---
+++ presentation/src/main/java/kr/hs/dgsw/stac/greenstreet/features/solution/view/AddSolutionFragment.kt
@@ -0,0 +1,48 @@
+package kr.hs.dgsw.stac.greenstreet.features.solution.view
+
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import dagger.hilt.android.AndroidEntryPoint
+import kr.hs.dgsw.stac.greenstreet.R
+import kr.hs.dgsw.stac.greenstreet.base.BaseFragment
+import kr.hs.dgsw.stac.greenstreet.databinding.FragmentAddSolutionBinding
+import kr.hs.dgsw.stac.greenstreet.features.solution.vm.AddSolutionViewModel
+
+@AndroidEntryPoint
+class AddSolutionFragment : BaseFragment<FragmentAddSolutionBinding, AddSolutionViewModel>(R.layout.fragment_add_solution) {
+
+    override val viewModel: AddSolutionViewModel by viewModels()
+
+    private val navArgs: AddSolutionFragmentArgs by navArgs()
+
+    override fun start() {
+        observeViewEvent()
+        viewModel.postingId.value = navArgs.postingId
+
+    }
+
+    private fun observeViewEvent() = bindingViewEvent { event ->
+
+        when (event) {
+            AddSolutionViewModel.EVENT_SUCCESS_ADD_SOLUTION -> {
+                navigateToBack()
+            }
+
+            AddSolutionViewModel.EVENT_ON_CLICK_BACK -> {
+                navigateToBack()
+            }
+
+            AddSolutionViewModel.EVENT_ON_CLICK_ADD -> {
+                viewModel.addSolution()
+            }
+
+        }
+
+    }
+
+    private fun navigateToBack() {
+        findNavController().popBackStack()
+    }
+
+}
---
+++ presentation/src/main/java/kr/hs/dgsw/stac/greenstreet/features/solution/vm/AddSolutionViewModel.kt
@@ -0,0 +1,51 @@
+package kr.hs.dgsw.stac.greenstreet.features.solution.vm
+
+import androidx.lifecycle.MutableLiveData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kr.hs.dgsw.stac.domain.model.solution.SolutionType
+import kr.hs.dgsw.stac.domain.usecase.solution.CreateSolution
+import kr.hs.dgsw.stac.domain.usecase.solution.SolutionUseCases
+import kr.hs.dgsw.stac.greenstreet.base.BaseViewModel
+
+@HiltViewModel
+class AddSolutionViewModel @Inject constructor(
+    private val solutionUseCases: SolutionUseCases,
+
+) : BaseViewModel() {
+
+    val postingId = MutableLiveData<Long>()
+    val url = MutableLiveData<String>()
+    val title = MutableLiveData<String>()
+    val content = MutableLiveData<String>()
+
+    fun addSolution() {
+        solutionUseCases.createSolution.execute(
+            CreateSolution.Params(
+                postingId = postingId.value ?: return,
+                url = url.value ?: return,
+                type = SolutionType.IMAGE
+            )
+        )
+            .toObservable()
+            .subscribe(
+                { viewEvent(EVENT_SUCCESS_ADD_SOLUTION) },
+                { error -> onError.value = error }
+            )
+    }
+
+    fun onClickAdd() {
+        viewEvent(EVENT_ON_CLICK_ADD)
+    }
+
+    fun onClickBack() {
+        viewEvent(EVENT_ON_CLICK_BACK)
+    }
+
+    companion object {
+        const val EVENT_SUCCESS_ADD_SOLUTION = 0
+        const val EVENT_ON_CLICK_ADD = 1
+        const val EVENT_ON_CLICK_BACK = 2
+    }
+
+}
