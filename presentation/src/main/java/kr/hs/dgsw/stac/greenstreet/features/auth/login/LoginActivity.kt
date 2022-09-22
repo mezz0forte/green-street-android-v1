ab27cd9fa6e9c1b8ee1d404979b9f7fe92fb4c45
LucyMoon <rjf5907@daum.net>
2022-09-22 16:08:55 +0900

    Add: 토큰 저장

--- presentation/src/main/java/kr/hs/dgsw/stac/greenstreet/features/auth/login/LoginActivity.kt
+++ presentation/src/main/java/kr/hs/dgsw/stac/greenstreet/features/auth/login/LoginActivity.kt
@@ -10,6 +10,7 @@ import kr.hs.dgsw.stac.greenstreet.base.BaseActivity
 import kr.hs.dgsw.stac.greenstreet.databinding.ActivityLoginBinding
 import kr.hs.dgsw.stac.greenstreet.features.auth.register.RegisterActivity
 import kr.hs.dgsw.stac.greenstreet.features.main.MainActivity
+import kr.hs.dgsw.stac.greenstreet.widget.GreenStreetApplication
 
 @AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(R.layout.activity_login) {
@@ -39,6 +40,8 @@ class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(R.layou
     private fun observeLiveData() = with(viewModel) {
         token.observe(this@LoginActivity) {
             Log.d("TAGTAG", token.value!!.access_token)
+            GreenStreetApplication.prefs.setAccessToken(token.value!!.access_token)
+            GreenStreetApplication.prefs.setRefreshToken(token.value!!.refresh_token)
             val intent = Intent(applicationContext, MainActivity::class.java)
             startActivity(intent)
         }
